866380f8ef0d7661b69ec35971edcd939abfc3aa
ArDezZz <aromudez@gmail.com>
2022-07-13 18:51:25 +0300

    lesson1 task1

--- lesson1/task/src/App.jsx
+++ lesson1/task/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ThemedButton from './ThemedButton';
+import { themes, ThemeContext } from './theme-context';
 
-const App = () => {
-  return (
-    <div>
-      <button class="btn">Dynamic Theme</button>
-      <button class="btn">Default Theme</button>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    theme: themes.light,
+  };
 
+  toogleTheme = () => {
+    const newTheme = this.state.theme === themes.dark ? themes.light : themes.dark;
+    this.setState({
+      theme: newTheme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toogleTheme}>Dynamic Theme</ThemedButton>
+        </ThemeContext.Provider>
+        <ThemedButton onClick={this.toogleTheme}>Default Theme</ThemedButton>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ lesson1/task/src/ThemedButton.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { ThemeContext } from './theme-context';
+
+class ThemedButton extends Component {
+  render() {
+    return (
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      />
+    );
+  }
+}
+ThemedButton.contextType = ThemeContext;
+export default ThemedButton;

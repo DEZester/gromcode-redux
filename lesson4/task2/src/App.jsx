d40e5f95503ad97b2e2b99bd5b2f26656d4400f7
ArDezZz <aromudez@gmail.com>
2022-07-24 19:08:26 +0300

    lesson4 task2

--- lesson4/task2/src/App.jsx
+++ lesson4/task2/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import store from './store';
+import Users from './users/Users';
 const App = () => {
-  return <div>Hello, React!</div>;
+  return (
+    <Provider store={store}>
+      <Users />
+    </Provider>
+  );
 };
 
 export default App;

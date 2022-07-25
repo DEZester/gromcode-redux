d297208f65df485b22b6593db06f4d80d5d71b3c
ArDezZz <aromudez@gmail.com>
2022-07-25 15:54:30 +0300

    pagination fix

--- lesson4/hw1/src/users/Pagination.jsx
+++ lesson4/hw1/src/users/Pagination.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Pagination = ({ goNext, goPrev, currentPage, itemsPerPage, totalItems }) => {
   const isPrevPageAvailable = currentPage > 1;
-  const isNextPageAvailable = totalItems - itemsPerPage * currentPage > 0;
-
+  const isNextPageAvailable =
+    Math.ceil(totalItems / itemsPerPage) > currentPage || totalItems === 0;
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
-        {!isPrevPageAvailable ? '' : '←'}
+        {isPrevPageAvailable ? '←' : ''}
       </button>
       <span className="pagination__page">{currentPage}</span>
       <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
-        {!isNextPageAvailable ? ' ' : '→'}
+        {isNextPageAvailable ? '→' : ''}
       </button>
     </div>
   );

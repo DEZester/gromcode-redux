4cd8b113ea6a514c1d8ffcba87d389d3865a6545
ArDezZz <aromudez@gmail.com>
2022-07-29 13:52:31 +0300

    prepare to task2 lesson6

--- lesson6/task2/src/App.jsx
+++ lesson6/task2/src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return (
+    <div class="page">
+      <div class="user">
+        <img
+          alt="User Avatar"
+          src="https://avatars0.githubusercontent.com/u/10639145?v=4"
+          class="user__avatar"
+        />
+        <div class="user__info">
+          <span class="user__name">Apple</span>
+          <span class="user__location">Cupertino, CA</span>
+        </div>
+      </div>
+
+      <span class="spinner"></span>
+
+      <div class="name-form">
+        <input type="text" class="name-form__input" />
+        <button class="name-form__btn btn">Show</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;

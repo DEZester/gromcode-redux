43e9e5a452e01306a644619062b204789ef89f55
ArDezZz <aromudez@gmail.com>
2022-07-29 13:41:09 +0300

    lesson6 task1

--- lesson6/task1/src/store.js
+++ lesson6/task1/src/store.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import counterReducer from './counter.reducer';
 
-const store = createStore(counterReducer);
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info(action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const store = createStore(counterReducer, applyMiddleware(logger));
 
 export default store;
